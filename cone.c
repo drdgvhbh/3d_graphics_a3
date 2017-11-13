8576ce5004f6cc1bc16314aa4836621d1883a2a6
Unknown <drdgvhbh@gmail.com>
2017-11-13 16:45:49 -0500

    cone

--- cone.c
+++ cone.c
@@ -21,12 +21,51 @@ extern double Normalize();
  * (ie my intersection point is further than something else already hit).
  */
 
-int IntersectCone(ray, t, normal)
-	Ray *ray;
-	double *t;
-	Vector *normal;
-{
+int IntersectCone(Ray* ray, double* t, Vector* normal) {
 	/* your code goes here */
+	double a = 
+		pow(ray->direction.v[0], 2)
+		- pow(ray->direction.v[1], 2)
+		+ pow(ray->direction.v[2], 2);
+	double b = 
+		2 *
+		(
+			(ray->origin.v[0] * ray->direction.v[0])
+			- (ray->origin.v[1] * ray->direction.v[1])
+			+ ray->origin.v[2] * ray->direction.v[2]
+		);
+	double c = 
+		pow(ray->origin.v[0], 2)
+		-pow(ray->origin.v[1], 2)
+		+ pow(ray->origin.v[2], 2);
+
+	double d, myT;
+	double r1, r2;
+	Point hit;
 	
-	return MISS;
+	d= b*b-4.0*a*c;
+	if(d <= 0.0)
+		return MISS;
+	
+	if(b < 0.0) {
+		r2= (-b+sqrt(d))/(2.0*a);
+		r1= c/(a*r2);
+	} else {
+		r1= (-b-sqrt(d))/(2.0*a);
+		r2= c/(a*r1);
+	}
+	if(r1 < EPSILON)
+		if(r2 < EPSILON)
+			return MISS;
+		else	myT= r2;
+	else	myT= r1;
+
+	if(myT >= *t)
+		return MISS;
+	TIMES(hit,  ray->direction, myT);
+	PLUS(hit, ray->origin, hit);
+	*t= myT;
+	*normal= hit;
+
+	return HIT;
 }
